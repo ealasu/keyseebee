5ac4fee03708695a704d800c3af61c42f38c0728
ealasu <ealasu@users.noreply.github.com>
2020-10-12 15:30:19 -0700

    compiles

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -4,9 +4,26 @@
 // set the panic handler
 use panic_halt as _;
 
+pub use atsamd_hal as hal;
+use atsamd_hal::{
+    clock::GenericClockController,
+    define_pins,
+    gpio::{self, Floating, Input, IntoFunction, Output, Pa10, Pa11, PfC, Port, PullUp, PushPull},
+    prelude::*,
+    sercom::{PadPin, Sercom0Pad2, Sercom0Pad3, UART0},
+    target_device::{
+        self,
+        gclk::{clkctrl::GEN_A, genctrl::SRC_A},
+        interrupt, CorePeripherals, Peripherals, TC3,
+    },
+    time::Miliseconds,
+    timer::TimerCounter,
+    usb,
+    usb::UsbBus,
+};
 use core::convert::Infallible;
-use embedded_hal::prelude::*;
 use embedded_hal::digital::v2::{InputPin, OutputPin};
+use embedded_hal::prelude::*;
 use generic_array::typenum::{U4, U6};
 use keyberon::{
     action::{k, l, m, Action, Action::*},
@@ -15,59 +32,15 @@ use keyberon::{
     key_code::KbHidReport,
     key_code::KeyCode::*,
     layout::{Event, Layout},
-    matrix::{Matrix, PressedKeys}
+    matrix::{Matrix, PressedKeys},
 };
 use nb::block;
 use rtic::app;
-pub use atsamd_hal as hal;
-use atsamd_hal::{
-    clock::GenericClockController,
-    define_pins,
-    gpio::{
-        self,
-        Port,
-        Floating,
-        Input,
-        Output,
-        IntoFunction,
-        PullUp,
-        PushPull,
-        Pa10,
-        Pa11,
-        PfC
-    },
-    prelude::*,
-    sercom::{PadPin, Sercom0Pad2, Sercom0Pad3, UART0},
-    //use hal::serial;
-    usb,
-    //use hal::{stm32, timers};
-    target_device::{
-        self,
-        gclk::{clkctrl::GEN_A, genctrl::SRC_A},
-        interrupt,
-        CorePeripherals,
-        Peripherals,
-        TC3,
-    },
-    usb::{
-        UsbBus,
-    },
-    timer::{
-        TimerCounter,
-    },
-    time::Miliseconds
-};
 use usb_device::{
     bus::UsbBusAllocator,
     class::UsbClass,
-    device::{
-        UsbDevice,
-        UsbDeviceState
-    },
+    device::{UsbDevice, UsbDeviceState},
 };
-
-//type UsbClass = keyberon::Class<'static, usb::UsbBusType, ()>;
-//type UsbDevice = usb_device::device::UsbDevice<'static, usb::UsbBusType>;
 
 trait ResultExt<T> {
     fn get(self) -> T;
@@ -80,53 +53,6 @@ impl<T> ResultExt<T> for Result<T, Infallible> {
         }
     }
 }
-
-macro_rules! map_array {
-    ([$($item:expr),+], |$item_name:ident| $f:expr) => ({
-	[$(
-	    {
-		let $item_name = $item;
-		$f
-	    }
-	),+]
-    })
-}
-
-// pub struct Cols(
-//     gpioa::PA15<Input<PullUp>>,
-//     gpiob::PB3<Input<PullUp>>,
-//     gpiob::PB4<Input<PullUp>>,
-//     gpiob::PB5<Input<PullUp>>,
-//     gpiob::PB8<Input<PullUp>>,
-//     gpiob::PB9<Input<PullUp>>,
-// );
-// impl_heterogenous_array! {
-//     Cols,
-//     dyn InputPin<Error = Infallible>,
-//     U6,
-//     [0, 1, 2, 3, 4, 5]
-// }
-
-// pub struct Rows(
-//     gpiob::PB0<Output<PushPull>>,
-//     gpiob::PB1<Output<PushPull>>,
-//     gpiob::PB2<Output<PushPull>>,
-//     gpiob::PB10<Output<PushPull>>,
-// );
-// impl_heterogenous_array! {
-//     Rows,
-//     dyn OutputPin<Error = Infallible>,
-//     U4,
-//     [0, 1, 2, 3]
-// }
-
-/// Number of rows per half
-const COLS: usize = 7;
-/// Number of columns per half
-const ROWS: usize = 4;
-
-// pub type Cols = [&'static dyn InputPin<Error = ()>; COLS];
-// pub type Rows = [&'static dyn OutputPin<Error = ()>; ROWS];
 
 define_pins!(
     /// Maps the pins to their arduino names and
@@ -160,7 +86,6 @@ define_pins!(
     pin col5 = a16, // 11
     pin col6 = a17, // 13
 );
-
 
 pub struct Cols(
     atsamd_hal::gpio::Pa14<atsamd_hal::gpio::Input<atsamd_hal::gpio::PullUp>>,
@@ -260,7 +185,6 @@ const APP: () = {
     fn init(mut c: init::Context) -> init::LateResources {
         static mut USB_BUS: Option<UsbBusAllocator<UsbBus>> = None;
 
-
         let mut clocks = GenericClockController::with_external_32kosc(
             c.device.GCLK,
             &mut c.device.PM,
@@ -273,24 +197,25 @@ const APP: () = {
             .get_gclk(GEN_A::GCLK2)
             .expect("Could not get clock 2");
 
-        let mut port = c.device.PORT.split().port;
+        let mut port = c.device.PORT.split();
         let matrix = Matrix::new(
             Cols(
-                c.device.PORT.split().pa14.into_pull_up_input(&mut port),
-                c.device.PORT.split().pa9.into_pull_up_input(&mut port),
-                c.device.PORT.split().pa8.into_pull_up_input(&mut port),
-                c.device.PORT.split().pa5.into_pull_up_input(&mut port),
-                c.device.PORT.split().pa6.into_pull_up_input(&mut port),
-                c.device.PORT.split().pa16.into_pull_up_input(&mut port),
-                c.device.PORT.split().pa17.into_pull_up_input(&mut port),
+                port.pa14.into_pull_up_input(&mut port.port),
+                port.pa9.into_pull_up_input(&mut port.port),
+                port.pa8.into_pull_up_input(&mut port.port),
+                port.pa5.into_pull_up_input(&mut port.port),
+                port.pa6.into_pull_up_input(&mut port.port),
+                port.pa16.into_pull_up_input(&mut port.port),
+                port.pa17.into_pull_up_input(&mut port.port),
             ),
             Rows(
-                c.device.PORT.split().pa19.into_push_pull_output(&mut port),
-                c.device.PORT.split().pa18.into_push_pull_output(&mut port),
-                c.device.PORT.split().pa7.into_push_pull_output(&mut port),
-                c.device.PORT.split().pa15.into_push_pull_output(&mut port),
+                port.pa19.into_push_pull_output(&mut port.port),
+                port.pa18.into_push_pull_output(&mut port.port),
+                port.pa7.into_push_pull_output(&mut port.port),
+                port.pa15.into_push_pull_output(&mut port.port),
             ),
-        ).unwrap();
+        )
+        .unwrap();
 
         // let mut rcc = c
         //     .device
@@ -302,32 +227,17 @@ const APP: () = {
         //     .pclk(24.mhz())
         //     .freeze(&mut c.device.FLASH);
 
-        // let mut peripherals = Peripherals::take().unwrap();
-        let mut pins = Pins::new(c.device.PORT);
-        // let gpioa = c.device.GPIOA.split(&mut rcc);
-        // let gpiob = c.device.GPIOB.split(&mut rcc);
-
-        // let usb = usb::Peripheral {
-        //     usb: c.device.USB,
-        //     pin_dm: c.device.PORT.a24, // gpioa.pa11,
-        //     pin_dp: c.device.PORT.a25 // gpioa.pa12,
-        // };
         let usb_clock = &clocks.usb(&gclk0).unwrap();
         let usb_bus = {
             *USB_BUS = Some(UsbBusAllocator::new(UsbBus::new(
                 usb_clock,
                 &mut c.device.PM,
-                // c.device.PORT.a24, // gpioa.pa11,
-                // c.device.PORT.a25, // gpioa.pa12,
-                pins.usb_dm.into_function(&mut pins.port),
-                pins.usb_dp.into_function(&mut pins.port),
-                c.device.USB
+                port.pa24.into_function(&mut port.port),
+                port.pa25.into_function(&mut port.port),
+                c.device.USB,
             )));
             USB_BUS.as_ref().unwrap()
         };
-        // *USB_BUS = Some(usb::UsbBusType::new(usb));
-        // let usb_bus = USB_BUS.as_ref().unwrap();
-
         let usb_class = keyberon::new_class(usb_bus, ());
         let usb_dev = keyberon::new_device(usb_bus);
 
@@ -339,63 +249,30 @@ const APP: () = {
         timer.start(1.ms());
         timer.enable_interrupt();
 
-        // let mut timer = timers::Timer::tim3(c.device.TIM3, 1.khz(), &mut rcc);
-        // timer.listen(timers::Event::TimeOut);
-
-        // let pb12: &gpiob::PB12<Input<Floating>> = &gpiob.pb12;
-        let is_left = pins.is_left.is_low().unwrap();
+        let is_left = port.pa23.into_pull_up_input(&mut port.port).is_low().unwrap();
         let transform: fn(Event) -> Event = if is_left {
             |e| e
         } else {
             |e| e.transform(|i, j| (i, 11 - j))
         };
 
-        let rx_pin: Sercom0Pad3<_> = pins
-            .rx
-            .into_pull_down_input(&mut pins.port)
-            .into_pad(&mut pins.port);
-        let tx_pin: Sercom0Pad2<_> = pins
-            .tx
-            .into_push_pull_output(&mut pins.port)
-            .into_pad(&mut pins.port);
+        let rx_pin: Sercom0Pad3<_> = port.pa11
+            .into_pull_down_input(&mut port.port)
+            .into_pad(&mut port.port);
+        let tx_pin: Sercom0Pad2<_> = port.pa10
+            .into_push_pull_output(&mut port.port)
+            .into_pad(&mut port.port);
 
         let uart_clk = clocks
             .sercom0_core(&gclk2)
             .expect("Could not configure sercom0 core clock");
-        let mut uart = UART0::new(
+        let uart = UART0::new(
             &uart_clk,
             9600.hz(),
             c.device.SERCOM0,
             &mut c.device.PM,
             (rx_pin, tx_pin),
         );
-
-        // let (pa9, pa10) = (gpioa.pa9, gpioa.pa10);
-        // let pins = cortex_m::interrupt::free(move |cs| {
-        //     (pa9.into_alternate_af1(cs), pa10.into_alternate_af1(cs))
-        // });
-        // let mut serial = serial::Serial::usart1(c.device.USART1, pins, 38_400.bps(), &mut rcc);
-        // serial.listen(serial::Event::Rxne);
-        // let (tx, rx) = serial.split();
-
-
-        // let pin_c0: atsamd_hal::gpio::Pa14<atsamd_hal::gpio::Input<atsamd_hal::gpio::PullUp>> = c.device.PORT.split().pa14.into_pull_up_input(&mut port);
-        // let pin_r0: atsamd_hal::gpio::Pa19<atsamd_hal::gpio::Output<atsamd_hal::gpio::OpenDrain>> = c.device.PORT.split().pa19.into_open_drain_output(&mut port);
-
-        // let mut row_pins = map_array!(
-        //     [pins.row0, pins.row1, pins.row2, pins.row3],
-        //     |pin| (&mut pin.into_open_drain_output(&mut pins.port)) as &mut dyn OutputPin<Error = ()>
-        // );
-        // let mut col_pins = map_array!(
-        //     [pins.col0, pins.col1, pins.col2, pins.col3, pins.col4, pins.col5, pins.col6],
-        //     |pin| (&mut pin.into_pull_up_input(&mut pins.port)) as &mut dyn InputPin<Error = ()>
-        // );
-
-        // let pin_c0:  = c.device.PORT.split().pa14.into_pull_up_input(&mut port);
-        // let pin_r0:  = c.device.PORT.split().pa19.into_open_drain_output(&mut port);
-
-        // let pa15 = gpioa.pa15;
-
 
         init::LateResources {
             usb_dev,
@@ -405,14 +282,12 @@ const APP: () = {
             matrix,
             layout: Layout::new(LAYERS),
             transform,
-            // tx,
-            // rx,
             uart,
         }
     }
 
     // TODO: was USART0
-    #[task(binds = SERCOM0, priority = 5, spawn = [handle_event], resources = [uart])]
+    #[task(binds = SERCOM0, priority = 2, spawn = [handle_event], resources = [uart])]
     fn rx(c: rx::Context) {
         static mut BUF: [u8; 4] = [0; 4];
 
@@ -470,8 +345,7 @@ const APP: () = {
             .map(c.resources.transform)
         {
             for &b in &ser(event) {
-                // let _v: () = c.resources.uart;
-                block!(c.resources.uart.write(b)).get();
+                block!(c.resources.uart.write(b).map_err(|_| nb::Error::<()>::WouldBlock));
             }
             c.spawn.handle_event(Some(event)).unwrap();
         }
